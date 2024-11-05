b7452fb6c6ea522769b5e21c8e623a3be4bc71db
Suneliya <149096669+sunnadi@users.noreply.github.com>
2024-11-05 09:02:21 +0500

    Add files via upload

---
+++ compositedom.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class FileSystemComponent
+{
+    public string Name;
+
+    protected FileSystemComponent(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void Display(int indent = 0);
+
+    public abstract int GetSize();
+}
+
+class File : FileSystemComponent
+{
+    private int Size;
+
+    public File(string name, int size) : base(name)
+    {
+        Size = size;
+    }
+
+    public override void Display(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent) + $"File: {Name}, Size: {Size} KB");
+    }
+
+    public override int GetSize()
+    {
+        return Size;
+    }
+}
+
+class Directory : FileSystemComponent
+{
+    private List<FileSystemComponent> Components = new List<FileSystemComponent>();
+
+    public Directory(string name) : base(name) { }
+
+    public void Add(FileSystemComponent component)
+    {
+        if (!Components.Contains(component))
+        {
+            Components.Add(component);
+        }
+        else
+        {
+            Console.WriteLine($"{component.Name} уже существует в папке {Name}.");
+        }
+    }
+
+    public void Remove(FileSystemComponent component)
+    {
+        if (Components.Contains(component))
+        {
+            Components.Remove(component);
+        }
+        else
+        {
+            Console.WriteLine($"{component.Name} не найден в папке {Name}.");
+        }
+    }
+
+    public override void Display(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent) + $"Directory: {Name}");
+        foreach (var component in Components)
+        {
+            component.Display(indent + 2);
+        }
+    }
+
+    public override int GetSize()
+    {
+        int totalSize = 0;
+        foreach (var component in Components)
+        {
+            totalSize += component.GetSize();
+        }
+        return totalSize;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        File file1 = new File("File1.txt", 500);
+        File file2 = new File("File2.txt", 300);
+        File file3 = new File("File3.txt", 800);
+
+        Directory folder1 = new Directory("Folder1");
+        Directory folder2 = new Directory("Folder2");
+        Directory rootFolder = new Directory("RootFolder");
+
+        folder1.Add(file1);
+        folder1.Add(file2);
+        folder2.Add(file3);
+        rootFolder.Add(folder1);
+        rootFolder.Add(folder2);
+
+        rootFolder.Display();
+        Console.WriteLine($"\nTotal Size of RootFolder: {rootFolder.GetSize()} KB");
+    }
+}
---
+++ facadedom.cs
@@ -0,0 +1,123 @@
+﻿using System;
+public class TV
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("TV is turned on.");
+    }
+
+    public void SelectChannel(int level)
+    {
+        Console.WriteLine($"TV channel is select to {level}.");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("TV is turned off.");
+    }
+}
+public class AudioSystem
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Audio system is turned on.");
+    }
+
+    public void SetVolume(int level)
+    {
+        Console.WriteLine($"Audio volume is set to {level}.");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Audio system is turned off.");
+    }
+}
+
+public class DVDPlayer
+{
+    public void Play()
+    {
+        Console.WriteLine("DVDPlayer is played.");
+    }
+
+    public void Pause()
+    {
+        Console.WriteLine($"DVDPlayer is paused.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("DVDPlayer is stopped.");
+    }
+}
+
+public class GameConsole
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("GameConsole is turned on.");
+    }
+
+    public void Play()
+    {
+        Console.WriteLine($"GameConsole is played.");
+    }
+
+}
+
+public class HomeTheaterFacade
+{
+    private TV _tv;
+    private AudioSystem _audioSystem;
+    private DVDPlayer _dvdplayer;
+    private GameConsole _gameconsole;
+
+    public HomeTheaterFacade(TV tv, AudioSystem audioSystem, DVDPlayer dvdplayer, GameConsole gameconsole)
+    {
+        _tv = tv;
+        _audioSystem = audioSystem;
+        _dvdplayer = dvdplayer;
+        _gameconsole = gameconsole;
+    }
+
+    public void StartMovie()
+    {
+        Console.WriteLine("Preparing to start the movie...");
+        _tv.TurnOn();
+        _tv.SelectChannel(5);
+        _audioSystem.TurnOn();
+        _audioSystem.SetVolume(8);
+        _dvdplayer.Play();
+        _gameconsole.TurnOn();
+        _gameconsole.Play();
+        Console.WriteLine("Movie started.");
+    }
+
+    public void EndMovie()
+    {
+        Console.WriteLine("Shutting down movie...");
+        _dvdplayer.Stop();
+        _audioSystem.TurnOff();
+        _tv.TurnOff();
+        Console.WriteLine("Movie ended.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        AudioSystem audio = new AudioSystem();
+        DVDPlayer dvdplayer = new DVDPlayer();
+        TV tv = new TV();
+        GameConsole gameconsole = new GameConsole();
+
+        HomeTheaterFacade homeTheater = new HomeTheaterFacade(tv, audio, dvdplayer, gameconsole);
+
+        homeTheater.StartMovie();
+
+        Console.WriteLine();
+
+        homeTheater.EndMovie();
+    }
+}
